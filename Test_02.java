feaeb531f95549059818f2a07d558f6ba25360bf
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-09-07 19:53:48 +0900

    char 정리

---
+++ Test_02.java
@@ -0,0 +1,28 @@
+
+public class Test_02 {
+
+	public static void main(String[] args) {
+//		int num;
+//		num = (int)17.5;	강제 형변환
+//		System.out.println(num);
+		
+//		char ch = 'A';
+//		System.out.println(ch);
+//		
+//		int code = ch;
+//		System.out.println((char)code);
+		
+//		int code = 65;
+//		System.out.println(code);
+//		System.out.println((char)code);
+		
+//		String str1 = "A";
+//		String str2 = "B";
+//		System.out.println(str1 + str2);
+		
+		char str1 = 'A';
+		char str2 = 'B';
+		System.out.println(str1 + str2);
+	}
+
+}
