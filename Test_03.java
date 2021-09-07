9e3dc6735dcc4858763c64995af1f9b72c32d429
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-09-07 19:54:03 +0900

    자동, 강제 형변환 정리

---
+++ Test_03.java
@@ -0,0 +1,13 @@
+
+public class Test_03 {
+
+	public static void main(String[] args) {
+//		String str1 = "A";
+//		System.out.println(str1 + 1);
+		
+		char ch = 'A';
+		System.out.println(ch + 1);
+		System.out.println((char)(ch + 1));
+	}
+
+}
