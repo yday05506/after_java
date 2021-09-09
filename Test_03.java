dccd8b6f99ab634f44e29995f412cdb9cd7d40a9
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-09-09 23:48:05 +0900

    Call by reference 확인

--- Test_03.java
+++ Test_03.java
@@ -8,6 +8,35 @@ public class Test_03 {
 		char ch = 'A';
 		System.out.println(ch + 1);
 		System.out.println((char)(ch + 1));
+		
+//		int num;
+//		num = (int)17.5;
+//		System.out.println(num);
+		
+//		char ch = 'A';
+//		System.out.println(ch);
+//		
+//		int code = ch;
+//		System.out.println((char)code);
+		
+//		int code = 65;
+//		System.out.println(code);
+//		System.out.println((char)code);
+		
+//		String str1 = "A";
+//		String str2 = "B";
+//		System.out.println(str1 + str2);
+		
+		String str1 = new String("Hi");
+		String str1 = new String("Hi");
+		System.out.println(str1 + str2);
+		
+		if(str1.equals(str2))
+			System.out.println("같다.");
+		else
+			System.out.println("다르다.");
+	
+
 	}
 
 }
