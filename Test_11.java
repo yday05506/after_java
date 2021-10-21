0fb93ea8975fd1df5f9dfc0accdb4a2419c72696
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-10-21 19:44:24 +0900

    시간 체크

---
+++ Test_11.java
@@ -0,0 +1,27 @@
+
+public class Test_11 {
+
+	public static void main(String[] args) {
+//		long startTime = System.currentTimeMillis();
+//		//System.out.println(startTime);
+//		
+//		for(long i = 0; i < 1000000000; i++) {
+//			
+//		}
+//		
+//		long endTime = System.currentTimeMillis();
+//		System.out.println("시작 시간 : " + startTime);
+//		System.out.println("종료 시간 : " + endTime);
+//		System.out.println("소요 시간 : " + (endTime-startTime));
+		
+		System.out.println("***** 카운트 다운 *****");
+		
+		for(int i = 10; i >= 0; i--) {
+			for(long j = 1; j <= 2000000000; j++) {
+				
+			}
+			System.out.println(i);
+		}
+	}
+
+}
