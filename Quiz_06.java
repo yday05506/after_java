1c7912fd846f48da001a0a028c493ef623d8baca
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-10-26 20:00:03 +0900

    [for] 문자열을 입력 받아 각 자리의 수의 합을 출력

--- Quiz_06.java
+++ Quiz_06.java
@@ -6,10 +6,17 @@ public class Quiz_06 {
 		//for문을 이용해서 문자형 숫자를 입력받아서 각 자리의 합을 구하시오.
 		Scanner sc = new Scanner(System.in);
 		
-		for(int i = 0; i <= 10; i++) {
-			String n = sc.next();
-			System.out.println(n);
-		}
+		int sum = 0;
+		
+		String str = sc.next();
+//		System.out.println(str.length());
+//		System.out.println(str.charAt(0));
+//		System.out.println(str.charAt(2));
+//		System.out.println(str.charAt(4));
+		
+		for(int i = 0; i < str.length(); i++) 
+			sum = sum + str.charAt(i) - '0';	//'0' 대신 48도 괜찮음
+		
+		System.out.println(sum);
 	}
-
 }
