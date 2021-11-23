ad5b7ea408d03edec0d9ee54e915465f6d3e78ba
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-11-23 19:27:22 +0900

    가위바위보 게임 (선생님)
    방법 2 : 중급자

--- Game_01.java
+++ Game_01.java
@@ -15,47 +15,84 @@ public class Game_01 {
 			System.out.println();
 			
 			//방법 1 : 초급자 코스
-			if(com == 1) {	//com = 가위
-				switch (you) {
-				case 1 : 
-					System.out.println("컴퓨터 : 가위    사용자 : 가위 ==> 비겼습니다.");
+//			if(com == 1) {	//com = 가위
+//				switch (you) {
+//				case 1 : 
+//					System.out.println("컴퓨터 : 가위    사용자 : 가위 ==> 비겼습니다.");
+//					break;
+//				case 2 : 
+//					System.out.println("컴퓨터 : 가위    사용자 : 바위 ==> 이겼습니다.");
+//					break;
+//				case 3 : 
+//					System.out.println("컴퓨터 : 가위    사용자 : 보 ==> 졌습니다.");
+//					break;
+//				}
+//			}
+//			else if (com == 2) {	//com = 바위
+//				switch (you) {
+//				case 1 : 
+//					System.out.println("컴퓨터 : 바위    사용자 : 가위 ==> 졌습니다.");
+//					break;
+//				case 2 : 
+//					System.out.println("컴퓨터 : 바위    사용자 : 바위 ==> 비겼습니다.");
+//					break;
+//				case 3 : 
+//					System.out.println("컴퓨터 : 바위    사용자 : 보 ==> 이겼습니다.");
+//					break;
+//				}
+//			}
+//			else if (com == 3) {	//com = 보
+//				switch (you) {
+//				case 1 : 
+//					System.out.println("컴퓨터 : 보    사용자 : 가위 ==> 이겼습니다.");
+//					break;
+//				case 2 : 
+//					System.out.println("컴퓨터 : 보    사용자 : 바위 ==> 졌습니다.");
+//					break;
+//				case 3 : 
+//					System.out.println("컴퓨터 : 보    사용자 : 보 ==> 비겼습니다.");
+//					break;
+//				}
+//			}
+			
+			//방법 2 : 중급자 코스
+			switch(com) {
+				case 1 :
+					System.out.print("컴퓨터 : 가위 \t");
 					break;
-				case 2 : 
-					System.out.println("컴퓨터 : 가위    사용자 : 바위 ==> 이겼습니다.");
+				case 2 :
+					System.out.print("컴퓨터 : 바위 \t");
 					break;
-				case 3 : 
-					System.out.println("컴퓨터 : 가위    사용자 : 보 ==> 졌습니다.");
+				case 3 :
+					System.out.print("컴퓨터 : 보 \t");
 					break;
-				}
 			}
-			else if (com == 2) {	//com = 바위
-				switch (you) {
-				case 1 : 
-					System.out.println("컴퓨터 : 바위    사용자 : 가위 ==> 졌습니다.");
+			switch(you) {
+				case 1 :
+					System.out.print("사용자 : 가위 \t");
 					break;
-				case 2 : 
-					System.out.println("컴퓨터 : 바위    사용자 : 바위 ==> 비겼습니다.");
+				case 2 :
+					System.out.print("사용자 : 바위 \t");
 					break;
-				case 3 : 
-					System.out.println("컴퓨터 : 바위    사용자 : 보 ==> 이겼습니다.");
+				case 3 :
+					System.out.print("사용자 : 보 \t");
 					break;
-				}
-			}
-			else if (com == 3) {	//com = 보
-				switch (you) {
-				case 1 : 
-					System.out.println("컴퓨터 : 보    사용자 : 가위 ==> 이겼습니다.");
+				default :
+					System.out.println("1~3 사이의 숫자만 입력해주세요");
 					break;
-				case 2 : 
-					System.out.println("컴퓨터 : 보    사용자 : 바위 ==> 졌습니다.");
-					break;
-				case 3 : 
-					System.out.println("컴퓨터 : 보    사용자 : 보 ==> 비겼습니다.");
-					break;
-				}
 			}
 			
-			System.out.println("계속하시겠습니까? (y or n) : ");
+			if((you == 1 && com == 3) || (you == 2 && com == 1) || (you == 3 && com == 1))
+				System.out.println("사용자가 이겼습니다.");
+			else if(you == com)
+				System.out.println("비겼습니다.");
+			else
+				System.out.println("컴퓨터가 이겼습니다.");
+			
+			
+			System.out.println();
+			
+			System.out.print("계속하시겠습니까? (y or n) : ");
 			choice = sc.next();	//choice = y, n
 			
 			if(choice.equals("n") || choice.equals("N"))
