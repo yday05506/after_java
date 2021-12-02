d0539650b36da389dad4c8c74676261c3d9a474f
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-12-02 18:48:20 +0900

    [class] 5명의 친구 이름을 배열에 입력 받아 짝수 번째에 있는 이름을 출력

--- Test_22.java
+++ Test_22.java
@@ -8,8 +8,14 @@ public class Test_22 {
 		Scanner sc = new Scanner(System.in);
 		String[] name = new String[5];
 		
+		System.out.println("5명의 친구 이름을 입력하세요");
+		
 		for(int i = 0; i < name.length; i++) {
+			System.out.print((i + 1) + "번째 친구 이름 : ");
 			name[i] = sc.next();
+		}
+		
+		for(int i = 0; i < name.length; i++) {
 			if(i == 1 || i == 3)
 				System.out.println(name[i]);
 		}
